402b04aa882e5afbdd20631154a66f9862c92214
edicoweb <soyedicoweb@gmail.com>
2022-02-08 08:10:55 -0500

    update

---
+++ trianguloIsosceles/isosceles.js
@@ -0,0 +1,11 @@
+function calcularPerimetro(lado1, lado2, base){
+    const perimetro = (lado*2)+base;
+}
+
+calcularPerimetroTriangulo(){
+    const obtVariable = document.getElementById("inputLado1");
+    const varIngresado = obtVariable.value;
+
+}
+
+calcularAreaTriangulo(){}
