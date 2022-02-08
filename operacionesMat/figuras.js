e7f0243de36375af7e9f13836e56acff66c3c965
edicoweb <soyedicoweb@gmail.com>
2022-02-07 22:41:14 -0500

    update

--- operacionesMat/figuras.js
+++ operacionesMat/figuras.js
@@ -6,5 +6,20 @@ function perimetroCuadrado(lado){
 
  function areaCuadrado(lado){
     return lado*lado;
- }
+} 
 
+function calculatePerimeterSquare(){
+   const obtenerDato = document.getElementById("inputCuadrado");
+   const valorIngresado = obtenerDato.value;
+
+   const perimetro = perimetroCuadrado(valorIngresado);
+   alert(perimetro);
+}
+
+function calculateAreaSquare(){
+   const obtenerDato = document.getElementById("inputCuadrado");
+   const valorIngresado = obtenerDato.value;
+
+   const area = areaCuadrado(valorIngresado);
+   document.write(area);
+}
