750f4c1769dac69e722d2adb3a9b0899e7d4c46a
edicoweb <soyedicoweb@gmail.com>
2022-02-08 23:29:20 -0500

    update

--- Area-Per-rectangulo/figura.js
+++ Area-Per-rectangulo/figura.js
@@ -14,10 +14,31 @@ function calcularRectangulo(){
     const datObtenidoAltura  = obtDatoAltura.value;
 
     const perimetro =  perimetroRectangulo(parseInt(datObtenidoBase), parseInt(datObtenidoAltura));
-    document.getElementById('per-resultado').innerHTML = perimetro;
+    document.getElementById('per-resultado').innerHTML = perimetro + " cm";
 
     const area = areaRectangulo(datObtenidoBase, datObtenidoAltura);
-    document.getElementById('area-resultado').innerHTML = area;
+    document.getElementById('area-resultado').innerHTML = area + " cm^2";
 }
 
+// *********************CIRCULO******************************
 
+function perimetroCirculo(radio){
+    const pi = 3.14;
+    return 2*pi*radio;
+}
+
+const areaCirculo = (radio) =>{
+    const pi = 3.14;
+    return pi*(radio*radio);   
+}
+
+function calcularCirculo(){
+    const obtDato = document.getElementById("inputRadioCirculo");
+    const datObtenido = obtDato.value;
+
+    const perimetroc = perimetroCirculo(datObtenido);
+    document.getElementById('per-resultado-C').innerHTML = perimetroc + " cm";
+
+    const areac = areaCirculo(datObtenido);
+    document.getElementById('area-resultadoC').innerHTML = areac + " cm^2";
+}
