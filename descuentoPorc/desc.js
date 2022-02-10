3c190ad5cc2c0234f1b129ef6d50ff4b8eec62f1
edicoweb <soyedicoweb@gmail.com>
2022-02-10 08:35:39 -0500

    update

--- descuentoPorc/desc.js
+++ descuentoPorc/desc.js
@@ -1,5 +1,15 @@
 const calcularPrecioDescuento = (precio, descuento) =>{
     const porcentajePrecioConDescuento = 100 - descuento;
     return (precio * porcentajePrecioConDescuento)/100;
+}
 
+function calcularPrecioFinalConDescuento(){
+    const obtDatoPrecio = document.getElementById("inputPrecio");
+    const valorPrecio = obtDatoPrecio.value;
+
+    const obtDatoDescuento = document.getElementById("inputDescuento");
+    const valorDescuento = obtDatoDescuento.value;
+
+    const precioFinalProducto = calcularPrecioDescuento(valorPrecio, valorDescuento);
+    document.getElementById('ouputPrecio').innerHTML =  "El precio final es: "+precioFinalProducto;
 }
