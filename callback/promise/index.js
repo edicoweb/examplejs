9f54b9658e5169dc8eb35f7e4f8c8bb7cd8c22c2
edicoweb <soyedicoweb@gmail.com>
2022-04-06 18:43:01 -0500

    update

--- callback/promise/index.js
+++ callback/promise/index.js
@@ -1,7 +1,7 @@
 // Example 1
 const greetStudent = () => {
     return new Promise((resolve, reject)=>{
-        if(false){
+        if(true){
             resolve('Hello Student');
         } else{
             reject('Ups! Error Estudent');
@@ -15,7 +15,7 @@ greetStudent()
 // Example 2
 const greetTeacher = () => {
     return new Promise((resolve, reject)=>{
-        if(false){
+        if(true){
             setTimeout(() => { resolve('Hello! Teacher')}, 3000);
         }else{
             const err = new Error('Ups! Error Teacher');
@@ -23,7 +23,48 @@ const greetTeacher = () => {
         }
     });
 };
-
 greetTeacher()
 .then(resolve => console.log(resolve))
 .catch(err => console.error(err))
+
+
+//execute two promise
+Promise.all([greetStudent(), greetTeacher()])
+.then(Response =>{
+    console.log('Array of results', Response);
+})
+.catch(err => {
+    console.error(err);
+})
+
+//Example 3 - async await
+const greet = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(()=>resolve('Hello! '), 300)
+        : reject(new Error('Ups! Error'))
+    });
+};
+
+const newGreet = async() => {
+    const someThing = await greet();
+    console.log(someThing);
+}
+
+console.log('Before');
+newGreet();
+console.log('After');
+
+const anotherFunction = async() =>{
+    try{
+        const something = await greet();
+        console.log(something);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+
+console.log('Before 1 ');
+newGreet();
+console.log('After 1');
